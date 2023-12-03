61cf73e89035c34963de8b55be1206d6b35b6575
june20088 <june20088@gmail.com>
2023-12-04 02:19:34 +0800

    Add new file

---
+++ password.py
@@ -0,0 +1,14 @@
+password = '123456'
+i = 3
+while i > 0:
+	i = i - 1
+	pwd = input('請輸入密碼: ')
+	if pwd == password:
+		print('登入成功!')
+		break
+	else:
+		if i > 0:
+			print('密碼錯誤!')
+			print('還有', i, '次機會!')
+		else:
+			print('密碼鎖定!')
